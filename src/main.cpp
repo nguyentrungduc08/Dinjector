63553124c2d4569a7921876d858ed91cd0561b71
hydra <nguyentrungduc08@gmail.com>
2018-04-14 18:58:58 +0700

    add parser parameter

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,43 @@
+/* create by Trung-Duc Nguyen
+ * to run....
+ * ./Dinjector -m [METHOD] -t [TARGET] -p [PPS] -f [FAKESOURCES]  
+ * - [METHOD]:          TCP/UDP.
+ * - [TARGET]:          either IP address of target or domain.
+ * - [PPS] :            from 0 to 100000. 
+ * - [FAKESOURCES] :    Y-for using fakesources.
+ *                      N-for do not fakesources.  
+ * 
+*/
+
+#include <bits/stdc++.h>
 #include <iostream>
+
 
 using namespace std;
 
-int main(){
-	
+bool parse_Input_Parameter();
+
+
+int main(int argc, char ** argv){
+    cout    << "num parameters: " << argc
+            << endl;
+    return 0;
 }
+
+
+bool 
+parse_Input_Parameter()
+{
+    if (argc < 9)
+        {
+            cout    << "Using ./Dijector "
+                    << "-m [METHOD] " 
+                    << "-t [TARGET] "
+                    << "-p [PPS] "
+                    << "-f [FAKESOURCES] "
+                    << endl;
+            return false;
+        }
+    
+{
+
