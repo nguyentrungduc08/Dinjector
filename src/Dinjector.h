0c710d01d3dc3cbfd1e49848d88298166d88ac34
hydra <nguyentrungduc08@gmail.com>
2018-04-20 00:07:07 +0700

    add resources

---
+++ src/Dinjector.h
@@ -0,0 +1,29 @@
+#ifndef DINJECTION_H
+#define DINJECTION_H
+
+#include <bits/stdc++.h>
+
+#include <unistd.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdint.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include <linux/if_packet.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <netinet/ether.h>
+#include <netinet/udp.h>
+
+#define rep(i,n)    for((int) i = 0; i < (int) n; ++i)
+#define X           first
+#define Y           second
+
+
+#endif /* DINJECTION_H */
